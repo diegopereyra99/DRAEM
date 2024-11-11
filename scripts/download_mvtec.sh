de6e9a1b7892ed67d73db2fce2381e43d58002a0
Diego Pereyra <diego.pereyra99@gmail.com>
2024-11-11 11:58:46 +0100

    Splitted download datasets script

--- scripts/download_mvtec.sh
+++ scripts/download_mvtec.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-mkdir datasets
-cd datasets
-# Download describable textures dataset
-wget https://www.robots.ox.ac.uk/~vgg/data/dtd/download/dtd-r1.0.1.tar.gz
-tar -xf dtd-r1.0.1.tar.gz
-rm dtd-r1.0.1.tar.gz
-
 mkdir mvtec
 cd mvtec
 # Download MVTec anomaly detection dataset
